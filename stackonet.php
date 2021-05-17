6182e973cb6ab547773351190a3a9f4c0577d2cc
Code With Shahroz <webstark03@gmail.com>
2021-05-18 04:35:20 +0530

    initial commit

---
+++ stackonet.php
@@ -0,0 +1,16 @@
+<?php
+/*  
+
+Plugin Name: Stackonet New
+Author: Stackonet
+Version: 1.0.0
+Author URI: http://tawheedoehsaan.org/stackonet
+Description: Woocommerce Add-on Plugin
+
+*/
+
+defined( 'ABSPATH') or die('You can not access this file, you silly human');
+define('WP_USE_THEMES', false);
+
+include( $_SERVER['DOCUMENT_ROOT'].'/stackonet/wp-load.php');
+global $wpdb;
